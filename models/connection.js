7177391695085fc282fa7631ee10276aac021283
BernardoFontesNKWEB <bernardo.miranda@nkweb.com.br>
2021-08-10 21:28:59 -0300

    projeto store manager

---
+++ models/connection.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+
+//ab
+const DB_NAME = 'StoreManager';
+
+const connection = () => {
+  return MongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit();
+    });
+};
+
+module.exports = connection;
